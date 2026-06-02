3b9fa551d4d2eafcdfba5a9779219498bd527678
zanancoulibaly41-max <zanancoulibaly41@gmail.com>
2026-06-02 21:30:29 +0900

    Add files via upload

---
+++ DOM2/script.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const colorBox = document.getElementById("color-box");
+    const changeColorBtn = document.getElementById("change-color-btn");
+
+    function getRandomColor() {
+        const letters = "0123456789ABCDEF";
+        let color = "#";
+
+        for (let i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+
+        return color;
+    }
+
+    changeColorBtn.addEventListener("click", () => {
+        colorBox.style.backgroundColor = getRandomColor();
+    });
+
+});
